7d0ab75abd0d58a57f719d1d721d77cc9c500134
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2020-01-28 14:57:47 +0100

    Adding stuff

--- .github/post_release.sh
+++ .github/post_release.sh
@@ -12,21 +12,21 @@ echo "Closing current milestone"
 curl -s --request PATCH -H "Authorization: Bearer $1" -H "Content-Type: application/json" https://api.github.com/repos/$2/milestones/$milestone_number --data '{"state":"closed"}'
 
 echo "Getting issues closed"
-issues_closed=`curl -s "https://api.github.com/repos/$2/issues?milestone=$milestone_number" | jq '.[] | "\(.title) (#\(.number))"'`
+issues_closed=`curl -s "https://api.github.com/repos/$2/issues?milestone=$milestone_number" | jq '.[] | "\(.title) (#\(.number))"'` | sed -e 's/^"\(.*\)"$/\1/g'
 echo $issues_closed
 
 echo -n "Getting release url: "
-release_url=`cat $GITHUB_EVENT_PATH | jq '.release.url'`
+release_url=`cat $GITHUB_EVENT_PATH | jq '.release.url'` | sed -e 's/"//g'`
 echo $release_url
 
 echo -n "Getting release body: "
-release_body=`cat $GITHUB_EVENT_PATH | jq '.release.body'`
+release_body=`cat $GITHUB_EVENT_PATH | jq '.release.body'` | sed -e 's/"//g'`
 echo $release_body
 
 echo -n "Updating release body: "
 release_body="${release_body}\r\n${issues_closed}"
 echo $release_body
-curl -i --request PATCH -H "Authorization: Bearer $1" -H "Content-Type: application/json" $release_url --data "{\"body\": $release_body}"
+curl -i --request PATCH -H "Authorization: Bearer $1" -H "Content-Type: application/json" $release_url --data "{\"body\": \"$release_body\"}"
 
 echo "Creating new milestone"
 curl -s --request POST -H "Authorization: Bearer $1" -H "Content-Type: application/json" "https://api.github.com/repos/$2/milestones" --data "{\"title\": \"$4\"}"
